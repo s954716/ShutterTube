714de3c5c931ced779cee57bf4f48a623c51899d
ryan.lu <ryan.lu@innosoft.com.tw>
2021-01-30 16:09:48 +0800

    debug for chrome

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>this is a test!</p>
+        <p>this is a test4!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
